73e4cdf1cc592cc2f6f0165851db2f7a582195fc
Pavel Darii <paveldarii@yahoo.com>
2022-04-19 00:34:50 -0400

    return all data organised by Country Name

--- backend/controllers/api/census-data.js
+++ backend/controllers/api/census-data.js
@@ -4,9 +4,15 @@ const sequelize = require("../../config/connection");
 
 router.get("/", async (req, res) => {
   //return all census data
-
+  const censusData = await sequelize.query(
+    `SELECT  countries.name, years.year, census_data.population
+    FROM census_data
+    LEFT JOIN countries ON census_data.country_id = countries.id
+    LEFT JOIN years ON census_data.year_id = years.id ORDER BY countries.name ASC`,
+    { type: QueryTypes.SELECT }
+  );
   res.setHeader("Content-Type", "application/json");
-  res.json({ ...result1 });
+  res.json({ ...censusData });
 });
 
 module.exports = router;
