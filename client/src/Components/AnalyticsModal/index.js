595d3dda08ded63f68acfa2c0afff2405ecd5679
Pavel Darii <paveldarii@yahoo.com>
2022-04-20 11:14:18 -0400

    adding mdb package

--- client/src/Components/AnalyticsModal/index.js
+++ client/src/Components/AnalyticsModal/index.js
@@ -1,24 +1,45 @@
-import React from "react";
-import { Button, Modal, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
+import {
+  MDBBtn,
+  MDBModal,
+  MDBModalDialog,
+  MDBModalContent,
+  MDBModalHeader,
+  MDBModalTitle,
+  MDBModalBody,
+  MDBModalFooter,
+} from "mdb-react-ui-kit";
 
-function AnalyticsModal(props) {
+export default function AnalyticsModal() {
+  const [basicModal, setBasicModal] = useState(true);
+
+  const toggleShow = () => setBasicModal(!basicModal);
+
   return (
     <>
-      <Button>Open modal</Button>
-      <Modal
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
+      <MDBBtn onClick={toggleShow}>LAUNCH DEMO MODAL</MDBBtn>
+      <MDBModal show={basicModal} setShow={setBasicModal} tabIndex="-1">
+        <MDBModalDialog>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>Modal title</MDBModalTitle>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={toggleShow}
+              ></MDBBtn>
+            </MDBModalHeader>
+            <MDBModalBody>...</MDBModalBody>
+
+            <MDBModalFooter>
+              <MDBBtn color="secondary" onClick={toggleShow}>
+                Close
+              </MDBBtn>
+              <MDBBtn>Save changes</MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
     </>
   );
 }
-export default AnalyticsModal;
